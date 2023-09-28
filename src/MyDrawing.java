0818ae55a282854b5e3d9dac7722fb0810abac9a
rhcastro <94096212+rocky-mtn-high@users.noreply.github.com>
2023-09-28 11:09:27 -0600

    Changed scaling

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -3,20 +3,20 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 public class MyDrawing extends JPanel {
 	private Cat cat;
-	public MyDrawing() 
-	{
+
+	public MyDrawing() {
 		// Add critters to the drawing
 		cat = new Cat();
 	}
+
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
-		cat.draw(g, 50, 50);
+		cat.draw(g, 100, 50);
 	}
-	
+
 	/**
 	 * @param args
 	 */
@@ -27,8 +27,8 @@ public class MyDrawing extends JPanel {
 		// Program will end when the window is closed
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// Set the size (otherwise only title bar displays)
-		frame.setSize(400, 400);
-		
+		frame.setSize(1600, 1600);
+
 		// Frame will not display until you set visible true
 		frame.setVisible(true);
 	}
